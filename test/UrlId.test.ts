ed89f30ebf2c7d9121af52673ca956faee536e85
samirthaker2020 <samir2009thaker@gmail.com>
2024-04-28 18:08:49 -0300

    chore(node-API): move files into models

--- test/UrlId.test.ts
+++ test/UrlId.test.ts
@@ -1,5 +1,5 @@
-import { UrlId } from '../src/UrlId';
-import { UrlIdTooShortError } from '../src/UrlIdTooShortError';
+import { UrlId } from '../src/models/UrlId';
+import { UrlIdTooShortError } from '../src/models/UrlIdTooShortError';
 
 describe('UrlId Test', () => {
   it('should create an instance of Secret Class', () => {
